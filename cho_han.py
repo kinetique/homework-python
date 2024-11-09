6f3bddb4fdd9c30f2d7ec67a1ab91a5b9ca15cec
kinetique <sonyacat0423@gmail.com>
2024-11-09 15:45:34 +0200

    class Players, constructor, bet and won_money functions

--- cho_han.py
+++ cho_han.py
@@ -1,11 +1,21 @@
 class Player:
-    pass
+    def __init__(self, players_money=5000):
+        self.money = players_money
+
+    def bet(self, amount):
+        return amount <= self.money
+
+    def won_money(self, amount):
+        self.money += amount
+
 
 class ChoHan:
     pass
 
+
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
