b01fdd84ce3d4fa1352c840fafe04027a8cdbe22
kinetique <sonyacat0423@gmail.com>
2024-11-09 16:00:59 +0200

    class ChoHan constructor, function for rolling dice where there are generating stats for dice and cho_hun_sum function that returns if the sum is cho or han

--- cho_han.py
+++ cho_han.py
@@ -1,3 +1,8 @@
+import random
+
+JAPANESE_NUMBERS = {1: 'ICHI', 2: 'NI', 3: 'SAN', 4: 'SHI', 5: 'GO', 6: 'ROKU'}
+
+
 class Player:
     def __init__(self, players_money=5000):
         self.money = players_money
@@ -10,7 +15,18 @@ class Player:
 
 
 class ChoHan:
-    pass
+    HOUSE_FEE = 40
+
+    def __init__(self):
+        self.player = Player()
+
+    def roll_dice(self):
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        return die1, die2
+
+    def cho_han_sum(self, die1, die2):
+        return 'cho' if (die1 + die2) % 2 == 0 else 'han'
 
 
 def main():
