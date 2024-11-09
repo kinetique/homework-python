27897b3da88f30e032b98b0fb2d86a1aefe5c1ee
kinetique <sonyacat0423@gmail.com>
2024-11-09 17:04:18 +0200

    class computer, constructor with comp memory and password picker and function checking the right letters in guesses

--- hacking.py
+++ hacking.py
@@ -1,5 +1,27 @@
+import random
+
+
 class Computer:
-    pass
+    def __init__(self):
+        self.memory = {
+            "0x1150": "RESOLVE",
+            "0x1250": "CHICKEN",
+            "0x1160": "ADDRESS",
+            "0x1260": "DESPITE",
+            "0x1180": "REFUGEE",
+            "0x1280": "DISPLAY",
+            "0x11a0": "PENALTY",
+            "0x12a0": "IMPROVE"
+        }
+
+        self.password = random.choice(list(self.memory.values()))
+
+    def check_guess(self, guess):
+        correct_count = 0
+        for i in range(len(self.password)):
+            if self.password[i] == guess[i]:
+                correct_count += 1
+        return correct_count
 
 
 class Hacker:
@@ -8,6 +30,7 @@ class Hacker:
 
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
