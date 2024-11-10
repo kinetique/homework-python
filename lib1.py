c4ce8ead1b3c518b51e4cc497f03ee758eb03db8
kinetique <sonyacat0423@gmail.com>
2024-11-10 21:00:08 +0200

    child classes attributes

--- lib1.py
+++ lib1.py
@@ -6,21 +6,38 @@ class Book:
         self.year_published = year_published
 
     def get_info(self):
-        return f"Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}, Year published: {self.year_published}"
+        return f"Title: {self.title},\nAuthor: {self.author},\nISBN: {self.isbn},\nYear published: {self.year_published}"
 
 
 class FictionBook:
-    pass
+    def __init__(self, title, author, isbn, year_published, genre)
+        super().__init__(title, author, isbn, year_published)
+        self.genre = genre
+
+    def get_info(self):
+        return f"{super().get_info()},\nGenre: {self.genre}"
 
 
 class NonFictionBook:
-    pass
+    def __init__(self, title, author, isbn, year_published, topic):
+        super().__init__(title, author, isbn, year_published)
+        self.topic = topic
+
+    def get_info(self):
+        return f"{super().get_info()},\nTopic: {self.topic}"
 
 
 class ReferenceBook:
-    pass
+    def __init__(self, title, author, isbn, year_published, description):
+        super().__init__(title, author, isbn, year_published)
+        self.description = description
+
+    def get_info(self):
+        return f"{super().get_info()},\nDescription: {self.description}"
 
 
 class Library:
     pass
 
+if __name__ == "__main__":
+    pass
