962243aaf46fc27d79f1d69e43000244b51aa633
kinetique <sonyacat0423@gmail.com>
2024-11-10 22:55:59 +0200

    class Piece and structure of its child classes and class Board

---
+++ chess1.py
@@ -0,0 +1,33 @@
+class Piece:
+    def __init__(self, color, x, y):
+        self.color = color
+        self.x = x
+        self.y = y
+
+
+class Pawn(Piece):
+    pass
+
+
+class Knight(Piece):
+    pass
+
+
+class Bishop(Piece):
+    pass
+
+
+class Rook(Piece):
+    pass
+
+
+class Queen(Piece):
+    pass
+
+
+class King(Piece):
+    pass
+
+
+class Board:
+    pass
