47c789ff9af4f5171539bb15864e6ae09bb98d73
kinetique <sonyacat0423@gmail.com>
2024-11-10 21:48:09 +0200

    adding books to classes and checking the functionality of all classes

--- lib1.py
+++ lib1.py
@@ -43,10 +43,10 @@ class Library:
     def add_book(self, book):
         self.books.append(book)
 
-    def remove_book(self, book):
-        self.books.remove(book)
+    def remove_book(self, isbn):
+        return [book for book in self.books if book.isbn != isbn]
 
-    def get_info(self):
+    def get_books(self):
         return [book.get_info() for book in self.books]
 
     def get_books_by_category(self, category):
@@ -59,5 +59,37 @@ class Library:
         return [book.get_info() for book in self.books if book.year_published == year_published]
 
 
+def main():
+    library = Library()
+
+    library.add_book(FictionBook("To Kill a Mockingbird", "Harper Lee", "9780061120084", 1960, "Drama"))
+    library.add_book(FictionBook("1984", "George Orwell", "9780451524935", 1949, "Dystopian"))
+    library.add_book(NonFictionBook("Educated", "Tara Westover", "9780399590504", 2018, "Memoir"))
+    library.add_book(NonFictionBook("The Immortal Life of Henrietta Lacks", "Rebecca Skloot", "9781400052189", 2010, "Biography"))
+    library.add_book(ReferenceBook("Gray's Anatomy", "Henry Gray", "9781455582327", 1858, "Medical reference for human anatomy"))
+
+    print("All books in library:")
+    print(library.get_info())
+
+    print ("Fiction books in library:")
+    print(library.get_books_by_category("Fiction"))
+
+    print ("Non-Fiction books in library:")
+    print(library.get_books_by_category("NonFiction"))
+
+    print ("Reference books in library:")
+    print(library.get_books_by_category("Reference"))
+
+    print("Books by Harper Lee:")
+    print(library.get_books_by_author("Harper Lee"))
+
+    print("Books published in 1949:")
+    print(library.get_books_by_year(1949))
+
+    library.remove_book("9780399590504")
+    print("All books in library after removal:")
+    print(library.get_books())
+
+
 if __name__ == "__main__":
-    pass
+    main()
