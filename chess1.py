5da6c8193a94d410e28ebc2331c7d7e81416b9b7
kinetique <sonyacat0423@gmail.com>
2024-11-10 22:59:27 +0200

    moving method in class Piece and __repr__ method to represent object as a string

--- chess1.py
+++ chess1.py
@@ -4,6 +4,13 @@ class Piece:
         self.x = x
         self.y = y
 
+    def move(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.color}, {self.x}, {self.y})'
+
 
 class Pawn(Piece):
     pass
